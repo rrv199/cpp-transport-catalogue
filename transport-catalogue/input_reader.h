b1bdc736cc47d20f528051f0689a351303ddd62a
rrv199 <168352193+rrv199@users.noreply.github.com>
2025-07-06 22:30:35 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,7 @@
-// место для вашего кода
+#pragma once
+#include "transport_catalogue.h"
+namespace TransportSystem::Input {
+Stop split_stop(std::string_view str);
+Bus split_bus(TransportCatalogue& catalogue, std::string_view str);
+void input_(TransportCatalogue& catalogue);
+}
