a72816dbf3610e697cf0e21d486870a08fc66a10
rrv199 <168352193+rrv199@users.noreply.github.com>
2025-07-08 22:44:37 +0300

    Update input_reader.cpp

--- transport-catalogue/input_reader.cpp
+++ transport-catalogue/input_reader.cpp
@@ -3,61 +3,53 @@
 #include <algorithm>
 
 namespace TransportSystem::Input {
-// Функция для парсинга строки с информацией об остановке
+
+// Разбирает строку с информацией об остановке (формат: "Stop <name>: <lat>, <lon>")
 Stop split_stop(std::string str) {
-    // Поиск разделителей в строке
     auto twopoint_pos = str.find(':');
     auto comma_pos = str.find(',');
-    auto entry_length = 5; // Длина префикса "Stop "
-    auto distance = 2;     // Расстояние между разделителем и данными
+    auto entry_length = 5; // "Stop "
+    auto distance = 2;  // Длина разделителя ": " и ", "
     
     Stop _stop;
-    
-    // Извлечение названия остановки (между "Stop " и ':')
     _stop.name_ = str.substr(entry_length, twopoint_pos - entry_length);
-    
-    // Парсинг широты (между ':' и ',')
     _stop.latitude_ = stod(str.substr(twopoint_pos + distance, comma_pos - twopoint_pos - distance));
-    
-    // Парсинг долготы (после запятой)
     _stop.longitude_ = stod(str.substr(comma_pos + distance));
     
     return _stop;
 }
 
-// Функция для парсинга маршрута автобуса
+// Разбирает строку с информацией о маршруте (формат: "Bus <name>: <stop1> > <stop2> > ...")
 Bus split_bus(TransportCatalogue& catalogue, std::string_view str) {
-    auto entry_length = 4; // Длина префикса "Bus "
-    auto distance = 2;     // Отступ от разделителя
+    auto entry_length = 4; // "Bus "
+    auto distance = 2;  // Длина разделителя ": " и " > "
     auto twopoint_pos = str.find(':');
     
     Bus bus;
-    // Извлечение названия маршрута (между "Bus " и ':')
     bus.name_ = str.substr(entry_length, twopoint_pos - entry_length);
     
-    str = str.substr(twopoint_pos + distance); // Обрезаем обработанную часть
+    str = str.substr(twopoint_pos + distance);
     
-    // Обработка двух типов маршрутов: кольцевые (с '>') и линейные (с '-')
+    // Обработка кольцевого маршрута (разделитель " - ")
     auto more_pos = str.find('>');
-    if (more_pos == std::string_view::npos) { // Линейный маршрут
+    if (more_pos == std::string_view::npos) {
         auto tire_pos = str.find('-');
         
-        // Парсим все остановки до разделителей '-'
         while (tire_pos != std::string_view::npos) {
             bus.stops_.push_back(catalogue.get_stop(str.substr(0, tire_pos - 1)));
             str = str.substr(tire_pos + distance);
             tire_pos = str.find('-');
         }
         
-        // Добавляем последнюю остановку
         bus.stops_.push_back(catalogue.get_stop(str.substr(0, tire_pos - 1)));
         size_t size_ = bus.stops_.size() - 1;
         
-        // Создаем обратный маршрут для линейных маршрутов
+        // Для некольцевого маршрута дублируем остановки в обратном порядке
         for (size_t i = size_; i > 0; i--) {
             bus.stops_.push_back(bus.stops_[i-1]);
         }
-    } else { // Кольцевой маршрут
+    } else {
+        // Обработка некольцевого маршрута (разделитель " > ")
         while (more_pos != std::string_view::npos) {
             bus.stops_.push_back(catalogue.get_stop(str.substr(0, more_pos - 1)));
             str = str.substr(more_pos + distance);
@@ -68,37 +60,38 @@ Bus split_bus(TransportCatalogue& catalogue, std::string_view str) {
     return bus;
 }
 
-// Основная функция обработки ввода
-void input_(TransportCatalogue& catalogue) {
+// Основная функция ввода данных в транспортный каталог
+void input_(TransportCatalogue& catalogue, std::istream& input) {
     std::string count;
-    std::getline(std::cin, count); // Чтение количества записей
+    std::getline(input, count); // Чтение количества запросов
     
-    if (count != "") {
+    if (!count.empty()) {
         std::string str;
-        std::vector<std::string> buses; // Буфер для маршрутов
+        std::vector<std::string> buses;
         int amount = stoi(count);
         auto bus_distance = 3; // Длина префикса "Bus"
         
-        // Первый проход: обработка всех остановок
+        // Чтение и обработка всех запросов
         for (int i = 0; i < amount; ++i) {
-            std::getline(std::cin, str);
+            std::getline(input, str);
             
-            if (str != "") {
+            if (!str.empty()) {
                 auto space_pos = str.find_first_not_of(' ');
-                str = str.substr(space_pos); // Удаление пробелов в начале
+                str = str.substr(space_pos);
 
+                // Разделение остановок и маршрутов
                 if (str.substr(0, bus_distance) != "Bus") {
-                    catalogue.add_stop(split_stop(str)); // Добавление остановки
+                    catalogue.add_stop(split_stop(str));
                 } else {
-                    buses.push_back(str); // Сохранение маршрута для последующей обработки
+                    buses.push_back(str);
                 }
             }
         }
         
-        // Второй проход: обработка маршрутов (после всех остановок)
+        // Обработка маршрутов после всех остановок
         for (auto bus : buses) {
             catalogue.add_bus(split_bus(catalogue, bus));
         }
     }
 }
-}
+} // namespace TransportSystem::Input
