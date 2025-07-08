f4283918b07a6a27ce72a45d19bc468a72535e31
rrv199 <168352193+rrv199@users.noreply.github.com>
2025-07-08 22:45:51 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -2,8 +2,16 @@
 #include "transport_catalogue.h"
 #include <algorithm>
 #include <vector>
+
 namespace TransportSystem::Stat {
+
+// Получить информацию об остановке
 void query_stop(TransportCatalogue& catalogue, std::string_view stop_name);
+
+// Обработать транспортный запрос
 void query_(TransportCatalogue& catalogue, std::string_view str);
+
+// Вывести статистику системы
 void output_(TransportCatalogue& catalogue);
-}
+
+} // namespace TransportSystem::Stat
