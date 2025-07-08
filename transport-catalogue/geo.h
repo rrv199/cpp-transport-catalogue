8582bf9168dee41876c5eb4843ece796d719116c
rrv199 <168352193+rrv199@users.noreply.github.com>
2025-07-08 22:44:14 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -1,32 +1,36 @@
-#pragma once 
-#include <cmath>  
+#pragma once
 
-// Структура для хранения географических координат
+#include <cmath>
+
+// Географические координаты (широта и долгота)
 struct Coordinates {
     double latitude;  // Широта в градусах
-    double longitude;  // Долгота в градусах
+    double longitude; // Долгота в градусах
     
-    // Оператор сравнения на равенство для координат
+    // Проверка на равенство координат
     bool operator==(const Coordinates& other) const {
-        return latitude == other.latitude  // Сравнение широт
-            && longitude == other.longitude;  // Сравнение долгот
+        return latitude == other.latitude && longitude == other.longitude;
     }
 };
 
-// Функция для вычисления расстояния между двумя точками на поверхности Земли
+// Радиус Земли в метрах
+constexpr double EARTH_RADIUS = 6371000;
+
+// Вычисление расстояния между двумя точками на сфере (Земле)
 inline double ComputeDistance(Coordinates start, Coordinates end) {
-    using namespace std; 
+    using namespace std;
     
-    // Проверка на совпадение начальной и конечной точек
-    if (!(start == end)) {
-        const double dr = 3.1415926535 / 180.;  // Константа для перевода градусов в радианы
-        
-        // Формула гаверсинуса для вычисления расстояния между двумя точками на сфере
-        return acos(sin(start.latitude * dr) * sin(end.latitude * dr) 
-                    + cos(start.latitude * dr) * cos(end.latitude * dr) 
-                    * cos(abs(start.longitude - end.longitude) * dr))  
-                    * 6371000;  
-    } else {
-        return 0.0;  
+    // Если точки совпадают, возвращаем 0
+    if (start == end) {
+        return 0.0;
     }
+
+    // Коэффициент перевода градусов в радианы
+    const double dr = 3.1415926535 / 180.;
+    
+    // Вычисление расстояния по формуле гаверсинусов
+    return acos(sin(start.latitude * dr) * sin(end.latitude * dr) +
+               cos(start.latitude * dr) * cos(end.latitude * dr) *
+               cos(abs(start.longitude - end.longitude) * dr)) *
+               EARTH_RADIUS;
 }
