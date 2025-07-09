628247bef052a1e48f11df46e0fc5c1ea8491367
rrv199 <168352193+rrv199@users.noreply.github.com>
2025-07-09 07:26:34 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -8,19 +8,19 @@ using namespace std;
 using namespace TransportSystem;
 
 int main() {
- try {
- // Создаем экземпляр каталога транспорта
- TransportCatalogue catalogue;
- 
- // Читаем и обрабатываем входные данные
- Input::input_(catalogue);
- 
- // Обрабатываем запросы
- Stat::output_(catalogue);
- 
- return 0;
- } catch (const exception& e) {
- cerr << "Произошла ошибка: " << e.what() << endl;
- return 1;
- }
+    try {
+        // Создаем экземпляр каталога транспорта
+        TransportCatalogue catalogue;
+        
+        // Читаем и обрабатываем входные данные
+        Input::input_(catalogue);
+        
+        // Обрабатываем запросы (добавлено Stat::)
+        Stat::output_(catalogue, cin, cout);
+        
+        return 0;
+    } catch (const exception& e) {
+        cerr << "Произошла ошибка: " << e.what() << endl;
+        return 1;
+    }
 }
