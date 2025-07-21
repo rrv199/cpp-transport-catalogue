ca3fbe001969e775ce346475ff8a1cd74a2bc6b8
rrv199 <168352193+rrv199@users.noreply.github.com>
2025-07-21 23:40:46 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,15 +1,10 @@
+
 #pragma once
 #include "transport_catalogue.h"
+#include <string_view>
 
 namespace TransportSystem::Input {
 
-    // Разбирает строку с данными об остановке (формат: "Название: широта, долгота")
-    Stop split_stop(std::string_view str);
-
-    // Разбирает строку с описанием маршрута (формат: "Название: stop1 > stop2 ...")
-    Bus split_bus(TransportCatalogue& catalogue, std::string_view str);
-
-    // Основной метод для ввода данных в транспортный каталог
-    void input_(TransportCatalogue& catalogue, std::istream& input = std::cin);
+void ParseAndLoadData(TransportCatalogue& catalogue, std::istream& input);
 
 } // namespace TransportSystem::Input
